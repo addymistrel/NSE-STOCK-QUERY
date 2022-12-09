d7b11ede26b1945a9f6e978d6375b22c38de5f1f
addymistrel <102992848+addymistrel@users.noreply.github.com>
2022-12-09 23:30:17 +0530

    Add files via upload

---
+++ NSE Stock Query/queries.py
@@ -0,0 +1,19 @@
+import sqlite3
+import pandas as pd
+conn = sqlite3.connect("database.db")
+c = conn.cursor()
+
+def query1(col):
+    c.execute('''select * from bhavcopy order by ((close-open)/open) desc limit 25 ''')
+    newdf = pd.DataFrame(c.fetchall(),columns=col)
+    newdf.to_csv("query1.csv",index=False)
+
+def query2(col):
+    c.execute('''select * from bhavcopy_30days order by ((close-open)/open) desc limit 750''')
+    newdff = pd.DataFrame(c.fetchall(),columns=col)
+    newdff.to_csv("query2.csv",index=False)
+
+def query3(col):
+    c.execute('''select * from bhavcopy_30days order by ((close-open)/open) desc limit 25 ''')
+    newdfff = pd.DataFrame(c.fetchall(),columns=col)
+    newdfff.to_csv("query3.csv",index=False)
